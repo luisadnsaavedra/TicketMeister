09d0189d93c47b0f7a96a62c0f91a057619f1a6b
luisadnsaavedra <luisadan.saavedradeltoro@gmail.com>
2017-11-26 13:06:39 +0000

    added simple contact page without a form and simple tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -11,4 +11,16 @@ class HomeControllerTest < ActionController::TestCase
     assert_select 'h1', 'TicketMeister'
     assert_select 'p', 'Here you can find and purchase tickets to all the music related events happening near you.'
   end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+
+    assert_template layout: 'application'
+
+    assert_select 'title', 'TicketMeister'
+    assert_select 'h1', 'Contact Us'
+    assert_select 'p', 'Complete the following form to get in touch with us.'
+  end
+
 end
