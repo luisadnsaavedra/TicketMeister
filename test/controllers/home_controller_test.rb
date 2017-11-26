d8af5e6a98b7c2f89e1b06b5a848719d2ef9ea32
luisadnsaavedra <luisadan.saavedradeltoro@gmail.com>
2017-11-26 13:03:35 +0000

    Added simple home_controller_test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,6 +4,11 @@ class HomeControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-  end
 
+    assert_template layout: 'application'
+
+    assert_select 'title', 'TicketMeister'
+    assert_select 'h1', 'TicketMeister'
+    assert_select 'p', 'Here you can find and purchase tickets to all the music related events happening near you.'
+  end
 end
