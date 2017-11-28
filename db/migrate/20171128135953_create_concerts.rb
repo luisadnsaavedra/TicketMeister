eae32b561562a06b0e33633a9572e07748e26fc0
luisadnsaavedra <luisadan.saavedradeltoro@gmail.com>
2017-11-28 14:12:09 +0000

    Changed Concert description field no nullable

--- db/migrate/20171128135953_create_concerts.rb
+++ db/migrate/20171128135953_create_concerts.rb
@@ -1,7 +1,7 @@
 class CreateConcerts < ActiveRecord::Migration
   def change
     create_table :concerts do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :description
 
       t.timestamps null: false
