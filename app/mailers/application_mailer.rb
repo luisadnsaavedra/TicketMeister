a8a48f4a007c7fb47cc64478b5a95c5e7aa3511a
ls00683 <ls00683@surrey.ac.uk>
2017-12-06 14:15:32 +0000

    Added ability to add Seats to a concert (which will later be transformed into buy tickets for a concert and associated to a user).

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
+  #TODO: check SendGrid as email solution
   default to: "support@ticketmeister.com", from: "contact@ticketmeister.com"
   layout 'mailer'
 end
--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,6 +1,6 @@
 class Concert < ActiveRecord::Base
   belongs_to :theater
-  has_many :seats #dependent: :destroy TODO
+  has_many :seats, dependent: :destroy #(destroy seats if the concert is cancelled)
   validates :title, :theater, presence: true
   #make concert impressionable and store the counter in DB
   is_impressionable :counter_cache => true
