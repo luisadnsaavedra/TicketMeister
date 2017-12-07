4932e8e17d7cc04a883af81c3ddad58f5c204f45
ls00683 <ls00683@surrey.ac.uk>
2017-12-07 11:52:07 +0000

    Added redirect methods after login and logout of users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  #Method to redirect users to their tickets page after successful login
+  def after_sign_in_path_for(resource)
+    seats_path
+  end
+
+  #Method to redirect users to the concerts list after successful logout
+  def after_sign_out_path_for(resource)
+    concerts_path
+  end
+
 end
