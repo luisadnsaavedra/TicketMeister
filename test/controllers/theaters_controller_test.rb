2ecaa8d8e7ac1be0fc3183240c4d22c840d4c3ae
ls00683 <ls00683@surrey.ac.uk>
2017-12-08 10:50:12 +0000

    Changed failing theater_controller_tests, missing tests for success if an admin tries to edit/ add/ destroy theaters

--- test/controllers/theaters_controller_test.rb
+++ test/controllers/theaters_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class TheatersControllerTest < ActionController::TestCase
 
   include Devise::Test::ControllerHelpers
-  
+
   setup do
     @theater = theaters(:one)
   end
@@ -32,21 +32,24 @@ class TheatersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should not get edit" do
     get :edit, id: @theater
-    assert_response :success
+    assert_response :redirect
+    assert_redirected_to theaters_path #must be an admin to edit theater
   end
 
-  test "should update theater" do
+  test "should not update theater" do
     patch :update, id: @theater, theater: { description: @theater.description, title: @theater.title }
-    assert_redirected_to theater_path(assigns(:theater))
+    assert_redirected_to theaters_path #(assigns(:theater))
   end
 
-  test "should destroy theater" do
-    assert_difference('Theater.count', -1) do
+  test "should not destroy theater" do
+    assert_difference('Theater.count', 0) do
       delete :destroy, id: @theater
     end
 
     assert_redirected_to theaters_path
   end
+
+  #test "should destroy theater", test "should update theater", test "should get edit"
 end
