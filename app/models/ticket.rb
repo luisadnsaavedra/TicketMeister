bb508b8ca32962cc2a6ec0a87ba751a805641b99
ls00683 <ls00683@surrey.ac.uk>
2017-12-11 11:32:17 +0000

    Added ticket dependence to the user account instead of seats because of stack errors, changed ticket model tests to check robustness

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,6 @@
 class Ticket < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :seat, dependent: :destroy
+  belongs_to :user, dependent: :destroy
+  belongs_to :seat #, dependent: :destroy -Caused error 'Stack level too deep'
+  #The user would instead receive an email and choose whether to change tickets for another concert or ask for refund
+  validates :user, :seat, presence: true
 end
--- test/models/ticket_test.rb
+++ test/models/ticket_test.rb
@@ -5,5 +5,21 @@ class TicketTest < ActiveSupport::TestCase
   #   assert true
   # end
 
-  #TODO: Ticket tests
+  setup do
+    @ticket = tickets(:one)
+    # @ticket2 = tickets2(:one)
+  end
+
+  test 'should not save empty ticket' do
+    ticket = Ticket.new
+
+    ticket.save
+    refute ticket.valid?
+  end
+
+  test 'should save valid ticket' do
+    @ticket.save
+    assert @ticket.valid?
+  end
+
 end
